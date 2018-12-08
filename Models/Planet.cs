91361f001f7cb32bcb404a0af4b8ee876219d60f
Stefanwullems <stefanwullems1998@gmail.com>
2018-12-08 19:14:09 +0100

    planet model extended

--- Models/Planet.cs
+++ Models/Planet.cs
@@ -4,6 +4,13 @@ namespace starwars_browser_server.Models
     {
         public string name { get; set; }
         public string climate { get; set; }
+        public string rotation_period { get; set; }
+        public string orbital_period { get; set; }
+        public string diameter { get; set; }
+        public string gravity { get; set; }
+        public string terrain { get; set; }
+        public string surface_water { get; set; }
+        public string population { get; set; }
     }
 
 }
